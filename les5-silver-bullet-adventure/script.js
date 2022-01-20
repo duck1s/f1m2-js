64e66d674c24c9aa000c484318de2b86fcd0b8a7
Duck1s <joeri.breedveld123@gmail.com>
2022-01-20 12:45:20 +0100

    les6 compleet

--- les5-silver-bullet-adventure/script.js
+++ les5-silver-bullet-adventure/script.js
@@ -13,14 +13,14 @@ let current_index = 0;
 
 let lokaties = [
     {
-        "titel":"Ingang Silver Bullet",
+        "titel":"0. Ingang Silver Bullet",
         "image":"img/0.jpg",
         "directions": {
             "zuid":1
         }
     },
     {
-        "titel":"Gang bij docentenkamer",
+        "titel":"1. Gang bij docentenkamer",
         "image":"img/1.jpg",
         "directions": {
             "noord":0,
@@ -30,50 +30,104 @@ let lokaties = [
         }
     },
     {
-        "titel":"plaats 2",
-        "image":"img/2.jpg"
+        "titel":"2. Gang voor de trap",
+        "image":"img/2.jpg",
+        "directions": {
+            "zuid":3,
+            "oost":1
+        }
     },
     {
-        "titel":"plaats 3",
-        "image":"img/3.jpg"
+        "titel":"3. Kleine ruimte",
+        "image":"img/3.jpg",
+        "directions": {
+            "noord":2
+        }
     },
     {
-        "titel":"plaats 4",
-        "image":"img/4.jpg"
+        "titel":"4. Docentenkamer",
+        "image":"img/4.jpg",
+        "directions": {
+            "noord":1,
+        }
     },
     {
-        "titel":"plaats 5",
-        "image":"img/5.jpg"
+        "titel":"5. Gang voor C 0.90",
+        "image":"img/5.jpg",
+        "directions": {
+            "noord":10,
+            "oost":6,
+            "west":1
+        }
     },
     {
-        "titel":"plaats 6",
-        "image":"img/6.jpg"
+        "titel":"6. Gang voor toiletten",
+        "image":"img/6.jpg",
+        "directions": {
+            "west":5,
+            "oost":7,
+            "zuid":11
+        }
     },
     {
-        "titel":"plaats 7",
-        "image":"img/7.jpg"
+        "titel":"7. Gang bij 0.93",
+        "image":"img/7.jpg",
+        "directions": {
+            "west":6,
+            "zuid":8,
+            "noord":9
+        }
     },
     {
-        "titel":"plaats 8",
-        "image":"img/8.jpg"
+        "titel":"8. Lokaal 0.93",
+        "image":"img/8.jpg",
+        "directions": {
+            "noord":7
+        }
     },
     {
-        "titel":"plaats 9",
-        "image":"img/9.jpg"
+        "titel":"9. Lokaal 0.92",
+        "image":"img/9.jpg",
+        "directions": {
+            "zuid":7,
+            "west":10
+        }
     },
     {
-        "titel":"plaats 10",
-        "image":"img/10.jpg"
+        "titel":"10. Lokaal 0.90",
+        "image":"img/10.jpg",
+        "directions": {
+            "oost":9,
+            "zuid":5
+        }
     },
     {
-        "titel":"plaats 11",
-        "image":"img/11.jpg"
+        "titel":"11. Toiletten",
+        "image":"img/11.jpg",
+        "directions": {
+            "noord":6
+        }
     }
 ];
 
 function show(index) {
     myTitle.innerHTML = lokaties[index].titel;
     myImage.src = lokaties[index].image;
+    current_index = index;
+
+    updateDirections();
+}
+
+function updateDirections() {
+    let possible = lokaties[current_index].directions;
+    let possible_keys = Object.keys(possible);
+    let button_keys = Object.keys(directionButtons);
+    for(const key of button_keys) {
+        directionButtons[key].style.visibility = "hidden";
+    }
+    for(const key of possible_keys) {
+        directionButtons[key].style.visibility = "visible";
+    }
 }
 
 function getInput() {
@@ -81,3 +135,10 @@ function getInput() {
     myInput.value = "";
     myInput.focus();
 }
+
+function goDirection(richting) {
+    let punt_index = lokaties[current_index].directions[richting];
+    show(punt_index);
+}
+
+show(0);
